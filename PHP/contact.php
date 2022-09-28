299edf98d4ff933bc1c3018104056ee6320b5d78
SkyriMeg <m-pilon@orange.fr  git config --global user.name SkyriMeggit config --global user.name Meggit config --global user.email m-pilon@orange.fr  git config --global user.email m-pilon@orange.fr>
2022-09-28 17:09:06 +0200

    formulaire_contact

---
+++ PHP/contact.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nous contacter</title>
+    <link href="../CSS/_contact.css" rel="stylesheet">
+</head>
+
+<body></body>
+<h1>Nous contacter</h1>
+<p>Si vous voulez nous contacter, vous pouvez remplir le formulaire ci-dessous.</p>
+<section class="contactGrid">
+    <div></div>
+    <form class="contactForm">
+        <label for="name">Nom:</label><br />
+        <input type="text" id="name" name="name" value="Wilder Ranger" /><br />
+        <label for="email">Email:</label><br />
+        <input type="email" id="email" name="email" value="ranger@wild.com" /><br />
+        <label for="message">Message:</label><br />
+        <textarea id="message" name="message"></textarea>
+        <br /><br />
+        <input type="submit" value="Submit" id="submit" />
+    </form>
+</section>
+</body>
+
+</html>
