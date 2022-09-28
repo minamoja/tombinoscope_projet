0532af6df4cb0cf93204ef2730739160a439cf6d
minamoja <devezmad@yahoo.fr>
2022-09-28 17:09:17 +0200

    modif 28-09

---
+++ PHP/_header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header</title>
+    <link href="../CSS/_header.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+
+    <header>
+        <img src="../Images/logo.png" class="logo" />
+
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="trombi.php">Trombinoscope</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+        </nav>
+
+
+    </header>
+
+
+</body>
+
+</html>
