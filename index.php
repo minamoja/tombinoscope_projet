320bd9fd6cf00476198ac944ac140d7326b9d87a
FlorianBetin <florian.betin.pro@gmail.com>
2022-09-28 14:39:55 +0200

    structure and index html

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trombinoscope PHPower</title>
+</head>
+<body>
+    <h1>
+        Team PHPower
+    </h1>
+    <div>
+            <img src="" alt="">
+        </div>
+    <section>
+        <h2> Welcome !</h2>
+        <article><p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure sapiente necessitatibus neque expedita esse, tempora dolorem aliquid nobis assumenda labore possimus vitae quos dolor numquam illum officiis est, quasi sequi.
+        </p></article>
+        <h2> About us</h2>
+        <article>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, vel similique unde, ullam vero exercitationem eum reprehenderit ea blanditiis quae adipisci. Placeat autem, sequi fugit eligendi explicabo dicta vel reiciendis?
+        </article>
+
+    </section>
+
+</body>
+</html>
