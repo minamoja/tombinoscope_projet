7137e96bcbf07a9d9a2a863f731d01040420aa92
marinevlrg <marine.valorge@gmail.com>
2022-09-29 14:22:08 +0100

    creation footer et MAJ header et couleur background plus claire

---
+++ _footer.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header</title>
+    <link href="../CSS/_footer.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+
+    <footer>
+        <div class="divfooter">
+            <img src="../Images/logoblanc.png" class="logofooter" />
+
+            <p class="footertext">A bientôt chez les wilders</p>
+        </div>
+
+    </footer>
+
+
+</body>
+
+</html>
--- _header.php
+++ _header.php
@@ -12,13 +12,13 @@
 <body>
 
     <header>
-        <img src="../Images/logo.png" class="logo" />
-
+        <img src="../Images/logoblanc.png" class="logo" />
         <nav>
             <ul>
-                <li><a href="index.php"><img src="../images/iconehome.png" class="headernavimg"/></a></li>
-                <li><a href="trombi.php"><img src="../images/iconephoto.png" class="headernavimg"/></a></li>
-                <li><a href="contact.php"><img src="../images/iconecontact.png" class="headernavimg"/></a></li>
+
+                <li><a href="index.php"><img src="../images/iconehome.png" class="headernavimg" /></a></li>
+                <li><a href="trombi.php"><img src="../images/iconephoto.png" class="headernavimg" /></a></li>
+                <li><a href="contact.php"><img src="../images/iconecontact.png" class="headernavimg" /></a></li>
             </ul>
         </nav>
 
