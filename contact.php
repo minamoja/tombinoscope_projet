6015b176b0d8c0ba7f6d83506c1c6bc0696944a0
Nicolas Montard <montardnico@gmail.com>
2022-09-29 19:53:16 +0200

    mise en page contact

--- contact.php
+++ contact.php
@@ -6,7 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nous contacter</title>
-    <link href="../CSS/style.css" rel="stylesheet">
+    <link href="CSS/style.css" rel="stylesheet">
+    <link href="CSS/_contact.css" rel="stylesheet">
 </head>
 
 <?php include "_header.php" ?>
@@ -14,19 +15,24 @@
 <body>
 <h1>Nous contacter</h1>
 <p>Si vous voulez nous contacter, vous pouvez remplir le formulaire ci-dessous.</p>
-<section class="contactGrid">
-    <div></div>
-    <form class="contactForm">
-        <label for="name">Nom:</label><br />
-        <input type="text" id="name" name="name" value="Wilder Ranger" /><br />
-        <label for="email">Email:</label><br />
-        <input type="email" id="email" name="email" value="ranger@wild.com" /><br />
-        <label for="message">Message:</label><br />
-        <textarea id="message" name="message"></textarea>
-        <br /><br />
-        <input type="submit" value="Submit" id="submit" />
-    </form>
-</section>
+  <form>
+    <section class="contactGrid">
+        <section class= contactName>
+          <label for="name">Nom:</label><br />
+          <input type="text" id="name" name="name" value="Wilder Ranger" /><br />
+        </section>
+        <section class= contactEmail>
+          <label for="email">Email:</label><br />
+          <input type="email" id="email" name="email" value="ranger@wild.com" /><br />
+        </section>
+        <div class="contactImage"></div>
+    </section>
+          <label for="message">Message:</label><br />
+          <textarea id="message" name="message"></textarea>
+          <br /><br />
+          <input type="submit" value="Submit" id="submit" />
+          
+      </form>
 </body>
 
 <?php include "_footer.php" ?>
