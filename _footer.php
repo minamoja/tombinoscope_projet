72e0b9ca9f4ffb1feadbb5f69523a86f967d7993
FlorianBetin <florian.betin.pro@gmail.com>
2022-09-30 18:56:57 +0200

    fixed URL and unwanted texts

--- _footer.php
+++ _footer.php
@@ -1,31 +1,19 @@
-
-<<<<<<< HEAD
 <html>
-=======
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>footer</title>
-    <link href="../CSS/_footer.css" rel="stylesheet" type="text/css" />
+    <link href="/CSS/style.css" rel="stylesheet" type="text/css" />
 </head>
-
->>>>>>> marine
 
 <footer>
     <div class="divfooter">
         <img src="/pictures/logoblancmini.png" class="logofooter" />
 
-<<<<<<< HEAD
-=======
         <div class="footertext">A bientôt chez les wilders</div>
     </div>
->>>>>>> marine
 
 </footer>
 
-<<<<<<< HEAD
-=======
-
->>>>>>> marine
 </html>
--- _header.php
+++ _header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../CSS/_header.css">
+    <link rel="stylesheet" href="/CSS/style.css">
     <script src="script.js" defer></script>
     <title>Navbar</title>
 </head>
