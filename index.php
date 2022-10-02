f9bad0120de4b59ae686d908f20210c966197ab1
minamoja <devezmad@yahoo.fr>
2022-10-02 18:33:14 +0200

    ajout de la page ranger.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,8 +8,8 @@
     <title>Trombinoscope PHPower</title>
     <link rel="stylesheet" href="/CSS/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Raleway&family=Varela+Round&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway&family=Varela+Round&display=swap" rel="stylesheet">
 </head>
 
 <?php include "_header.php" ?>
@@ -18,13 +19,15 @@
         Team PHPower
     </h1>
     <div>
-            <img src="/pictures/montageteam.png" alt="Notre super équipe au complet">
-        </div>
+        <img src="/pictures/montageteam.png" alt="Notre super équipe au complet">
+    </div>
     <section class="indexSection">
-        <h2> Welcome !</h2>
-        <article><p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure sapiente necessitatibus neque expedita esse, tempora dolorem aliquid nobis assumenda labore possimus vitae quos dolor numquam illum officiis est, quasi sequi.
-        </p></article>
+        <h2> <a href="ranger.php">Welcome !</a></h2>
+        <article>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure sapiente necessitatibus neque expedita esse, tempora dolorem aliquid nobis assumenda labore possimus vitae quos dolor numquam illum officiis est, quasi sequi.
+            </p>
+        </article>
         <h2> About us</h2>
         <article>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, vel similique unde, ullam vero exercitationem eum reprehenderit ea blanditiis quae adipisci. Placeat autem, sequi fugit eligendi explicabo dicta vel reiciendis?</p>
---
+++ ranger.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ranger</title>
+    <link href="CSS/style.css" rel="stylesheet">
+</head>
+<?php include "_header.php" ?>
+
+<body>
+    <h1>Ranger X</h1>
+    <div class="ranger">
+
+        <div class="portrait">
+            <img id="img-ranger" src="https://fakeimg.pl/250x250/" alt="Nom du Ranger">
+        </div>
+        <h2>Nom du ranger</h2>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est, consequuntur? Aut nostrum ullam magnam ab doloremque corporis quae, cum provident ducimus eius dolorum asperiores? Similique consequatur et
+        </p>
+    </div>
+
+</body>
+
+<?php include "_footer.php" ?>
+
+</html>
