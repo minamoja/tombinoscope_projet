bfabee58543cf0c22d1e7ffae7392ce5438e4f9f
FlorianBetin <florian.betin.pro@gmail.com>
2022-10-04 11:10:01 +0200

    details for contact ranger

--- _script.php
+++ _script.php
@@ -2,19 +2,19 @@
 
 $phpRangers = [
     'Jihef' => 'Passioné par la musique, Jihef est autodidacte en code. Formateur pasionné, il est aimé par tous ses élèves',
-    'Megane' => 2,
-    'Marine' => 3,
+    'Megane' => 'Passionnée de musique, multi-instrumentiste: particulièrement batteuse depuis 20 ans, et mordue de jeux vidéo !',
+    'Marine' => "Pour moi coder rime avec 'boire du thé'",
     'Nicolas' => 4,
     'Alain' => "Grand garçon ayant dépassé le demi-siècle, son expérience en matière de coding reste celle d'un infra-junior, ce qui lui permet de se prétendre jeune.",
     'Florian' => 'Réscapé d\'un monde fait de tentacules, Florian essaye maintenant de reconstruire sa vie en codant',
     'Alex' => 7, 
-    'Axel'=> 8,
+    'Axel'=> "Je suis comme Sanson, mon pouvoir de codeur est dans ma barbe. Je l'ai rasée...",
     'Emeric'=> 9,
     'Florent'=> 10,
     'Hugo'=> 11,
     'Jessica' => 12,
     'Maria' => 13,
-    'Maxence' => 14,
+    'Maxence' => 'Joyeux drille du fond de la classe, Maxence sait surprendre par sa bonne humeur et sa bienveillance',
     'Magali'=> 15,
  ];
 
