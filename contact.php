9e715425d70d65e217dbef5b1f1abd253907c63b
SkyriMeg <m-pilon@orange.fr  git config --global user.name SkyriMeggit config --global user.name Meggit config --global user.email m-pilon@orange.fr  git config --global user.email m-pilon@orange.fr>
2022-10-04 10:07:14 +0200

    JS message reçu dans contact

--- contact.php
+++ contact.php
@@ -2,38 +2,39 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nous contacter</title>
-    <link href="CSS/style.css" rel="stylesheet">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Nous contacter</title>
+  <link href="CSS/style.css" rel="stylesheet">
 </head>
 
 <?php include "_header.php" ?>
 
 <body>
-<h1>Nous contacter</h1>
-<p>Si vous voulez nous contacter, vous pouvez remplir le formulaire ci-dessous.</p>
-  <form>
+  <h1>Nous contacter</h1>
+  <p>Si vous voulez nous contacter, vous pouvez remplir le formulaire ci-dessous.</p>
+  <form id="contactForm">
     <section class="contactGrid">
-        <section class= contactName>
-          <label for="name">Nom:</label><br />
-          <input type="text" id="name" name="name" value="Wilder Ranger" /><br />
-        </section>
-        <section class= contactEmail>
-          <label for="email">Email:</label><br />
-          <input type="email" id="email" name="email" value="ranger@wild.com" /><br />
-        </section>
-        <div class="contactImage"></div>
+      <section class=contactName>
+        <label for="name">Nom:</label><br />
+        <input type="text" id="name" name="name" value="Wilder Ranger" /><br />
+      </section>
+      <section class=contactEmail>
+        <label for="email">Email:</label><br />
+        <input type="email" id="email" name="email" value="ranger@wild.com" /><br />
+      </section>
+      <div class="contactImage"></div>
     </section>
-          <label for="message">Message:</label><br />
-          <textarea id="message" name="message"></textarea>
-          <br /><br />
-          <input type="submit" value="Submit" id="submit" />
-          
-      </form>
+    <label for="message">Message:</label><br />
+    <textarea id="message" name="message"></textarea>
+    <br /><br />
+    <input type="submit" value="Submit" id="submit" />
+
+  </form>
 </body>
 
 <?php include "_footer.php" ?>
+<script src="script.js"></script>
 
 </html>
--- script.js
+++ script.js
@@ -4,3 +4,12 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 })
+
+const message =
+    "Votre message a été envoyé ! Nous vous répondrons dès que possible.";
+
+document.getElementById("contactForm");
+document.addEventListener("submit", function (event) {
+    event.preventDefault();
+    alert(message);
+});
