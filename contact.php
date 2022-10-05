0f2c1cb53785e00f4ed480302bf40ea137285b7e
marinevlrg <marine.valorge@gmail.com>
2022-10-05 15:05:01 +0100

    maj logo et header

--- contact.php
+++ contact.php
@@ -12,30 +12,6 @@
 
 <?php include "_header.php" ?>
 
-<<<<<<< HEAD
-<body class= contactBody>
-  <?php include "_header.php" ?>
-  <div class= page-container>
-    <div class= content-wrap>
-  <h1>Nous contacter</h1>
-  <p>Si vous voulez nous contacter, vous pouvez remplir le formulaire ci-dessous.</p>
-  <form id="contactForm" action="/validation.php" method="POST">
-    <section class="contactGrid">
-      <section class=contactName>
-        <label for="name">Nom:</label><br />
-        <input type="text" id="name" name="name" placeholder="Wilder Ranger" arial-label="nom" required /><br />
-      </section>
-      <section class=contactEmail>
-        <label for="email">Email:</label><br />
-        <input type="email" id="email" name="email" placeholder="ranger@wild.com" arial-label="email" required /><br />
-      </section>
-      <div class="contactImage"></div>
-    </section>
-    <label for="message">Message:</label><br />
-    <textarea id="message" name="message" placeholder="Votre message" arial-label="message" required></textarea>
-    <br /><br />
-    <input type="submit" value="Envoyer" id="submit" />
-=======
 <body class=contactBody>
   <div class=page-container>
     <div class=content-wrap>
@@ -59,7 +35,6 @@
         <input type="submit" value="Envoyer" id="submit" />
 
       </form>
->>>>>>> marine
 
 </body>
 
